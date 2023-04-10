6381ff579b545dd752da9c208627bbccd7cc0801
cashamirica <cashamirica@cashamiricas-MacBook-Air.local>
2023-04-10 14:41:56 -0500

    in-class

---
+++ CocoaPod1/CocoaPod1/CocoaPod1App.swift
@@ -0,0 +1,17 @@
+//
+//  CocoaPod1App.swift
+//  CocoaPod1
+//
+//  Created by cashamirica on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct CocoaPod1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
